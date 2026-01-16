bb55dc8cd1633945eaa8546c5c537357c1a73482
infotranslinelogistics-coder <info.translinelogistics@gmail.com>
2026-01-16 00:48:32 +0000

    idek

--- app.config.js
+++ app.config.js
@@ -1,5 +1,14 @@
 export default ({ config }) => ({
   ...config,
+
+  updates: {
+    url: "https://u.expo.dev/e31119de-8260-4772-9009-3797a3a3e06a",
+  },
+
+  runtimeVersion: {
+    policy: "appVersion",
+  },
+
   extra: {
     ...config.extra,
     SUPABASE_URL: process.env.SUPABASE_URL,
